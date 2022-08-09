7225107028b2e626b98ad1e6111eb93d811a4906
wendikardian <wendikardian@gmail.com>
2022-08-09 11:00:44 +0700

    ;;

--- VirtualPainter/Handtracker.py
+++ VirtualPainter/Handtracker.py
@@ -11,6 +11,15 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 
+# Step 7
+mpDraw = mp.solutions.drawing_utils
+
+# Step 9
+# Declare variable to calculate the FPS
+# Ptime means previous time and cTime means current time
+pTime = 0
+cTime = 0
+
 while True:
     res, frame = cap.read()
 
@@ -34,6 +43,33 @@ while True:
     if results.multi_hand_landmarks:
         # We will loop it, because maybe there is much more than 1 hand on the camera
         for handLms in results.multi_hand_landmarks:
+
+            # Step 12
+            # After to find landmark on the frame, next we need to figure out the id of the landmark
+            for id, lm in enumerate(handLms.landmark):
+                # Try to print it, this is the example of the what is the value of id and lm
+                # 20 x: 0.5787419676780701 -> 20 is id
+                # y: 0.9594467282295227
+                # z: -0.01320577971637249
+                # print(id, lm)
+                h, w, c = frame.shape #try to get height, width, channel
+                cx, cy = int(lm.x*w), int(lm.y*h)
+
+            # Step 6
+            # After we define the mpDraw to draw the solutions, use it to draw it
+            mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
+            # Step 8. Try to run this and then see the frame of the camera if there is red node on the hand, try it for multiple hand
+            # HAND_CONNECTIONS means it will connect every node so it has a connection to each other (similar with a graph)
+
+    # Step 10
+    # Calculate the time for display the FPS
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    # Step 11
+    # After find the FPS value we need to display it on the frame
+    cv2.putText(frame, str(int(fps)), (10,70), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
 
     cv2.imshow("Frame", frame)
 
